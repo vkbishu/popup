011468dfb5f38551a89f5ba995930690c4c37cd6
vkbishu <bishukumar007@gmail.com>
2022-05-27 09:26:13 +0530

    Page title update

--- src/components/layouts/BaseLayout.vue
+++ src/components/layouts/BaseLayout.vue
@@ -6,7 +6,7 @@
                     <slot name="sidebar"></slot>
                 </div>
                 <div class="col-md-8 h-100">
-                    <div class="white_box p-3 h-100 overflow-auto">
+                    <div class="grey_box p-3 h-100 overflow-auto" :class="{'white_box': hasNoActions}">
                         <slot></slot>
                     </div>
                 </div>
@@ -19,10 +19,11 @@
 
 export default {
     name: "BaseLayout",
-    components: {
-       
-    }
-   
+   computed:{
+       hasNoActions(){
+           return this.$store.state.actions.selectedActions.length === 0
+       }
+   }
 }
 </script>
 
